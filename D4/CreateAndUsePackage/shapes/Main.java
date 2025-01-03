535e6e4136f8e7b90e6f594b537f1eb744a85414
MukilanThangamani <159744796+MukilanThangamani@users.noreply.github.com>
2025-01-03 14:27:58 +0530

    Add files via upload

---
+++ D4/CreateAndUsePackage/shapes/Main.java
@@ -0,0 +1,11 @@
+import CreateAndUsePackage.shapes.Circle;
+import CreateAndUsePackage.shapes.Rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle obj = new Circle();
+        obj.areaOfCircle(2);
+        Rectangle obj2 = new Rectangle();
+        obj2.areaOfRectangle(2,3);
+    }
+}
