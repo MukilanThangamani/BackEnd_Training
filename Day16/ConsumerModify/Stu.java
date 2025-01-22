10b21003f2adbf3d878b82c24f27ed6034f86b8d
MukilanThangamani <159744796+MukilanThangamani@users.noreply.github.com>
2025-01-22 08:52:01 +0530

    Add files via upload

---
+++ Day16/ConsumerModify/Stu.java
@@ -0,0 +1,19 @@
+package ConsumerModify;
+
+public class Stu {
+    private String name;
+    private  int grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public Stu(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
